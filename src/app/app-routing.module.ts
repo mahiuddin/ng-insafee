47687144c08d776ad770dd48fe193dd51f2f0bfe
mahiuddin <mahi.ewu@gmail.com>
2021-05-25 16:31:04 +0800

    product crud operationn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [];
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,19 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  product: Product = {} as Product; 
+  products: Product[] = [];
+  currentIndex:number=1;
+  product: Product = {} as Product;
+   
   
   constructor() { }
 
   ngOnInit(): void {
+    this.reload();
+  }
+
+  reload(){
+    const products = localStorage.getItem("product") ?? JSON.stringify({} as Product);
+    this.products = JSON.parse(products);
   }
 
   saveProduct(){
-    localStorage.setItem("product", JSON.stringify(this.product))
+    if(this.product.id > 0){
+      localStorage.setItem("product", JSON.stringify(this.products));
+      var product = {} as Product;
+      this.product = product;
+    }else{
+    this.product.id = this.currentIndex;
+    this.products.push(this.product);
+    localStorage.setItem("product", JSON.stringify(this.products));
+    var product = {} as Product;
+    this.product = product;
+    this.currentIndex++;
+    }
+    
+  }
+
+  editProduct(product: Product){
+    this.product = product;
+  }
+
+  deleteProduct(product:Product){
+    const index = this.products.indexOf(product);
+    this.products.splice(index,1);
+    localStorage.setItem("product", JSON.stringify(this.products));
+  }
+
+  clearProduct(){
+    localStorage.clear();
+    this.product={} as Product;
   }
 }
 
 export interface Product{
+   id:number;
    name : string;
    description: string;
    price: number;
